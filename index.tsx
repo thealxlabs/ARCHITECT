2b9ea1c9fe03ebc22ba671b13da1632c84c1e0e3
Alexander Wondwossen <alex.reed0@icloud.com>
2026-03-30 08:10:29 -0400

    Add PostHog analytics (US cloud)

--- index.tsx
+++ index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import posthog from 'posthog-js';
 import App from './App';
+
+posthog.init('phx_3sdL9hVErfxHHMwrPRucB0s3GtdpaEjFTpBVC9dNcbWIDC', {
+  api_host: 'https://us.i.posthog.com',
+  person_profiles: 'identified_only',
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
